0d594338dbccbdea20699e48fd4eeae3eff27106
Edigar Maia <edigarmaia@gmail.com>
2023-07-22 11:16:54 -0300

    Estrutura de repeticao for com range

---
+++ python/estrutura_repeticao_for_range.py
@@ -0,0 +1,13 @@
+print(list(range(10)))
+print()
+
+for numero in range(0,10):
+    print(numero, end=" ")
+print() # quebra de linha
+
+
+# range(start, stop, step)
+# exemplo utilizando a função built-in range
+for numero in range(0,51, 5):
+    print(numero, end=" ")
+print()
