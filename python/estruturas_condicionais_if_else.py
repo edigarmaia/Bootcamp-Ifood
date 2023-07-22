2a757fba12a8751acf4a99803d5caa708f78e67c
Edigar Maia <edigarmaia@gmail.com>
2023-07-22 10:03:44 -0300

    Estrutura condicional if else

---
+++ python/estruturas_condicionais_if_else.py
@@ -0,0 +1,7 @@
+MAIOR_IDADE = 18
+idade = int(input("Informe sua idade: "))
+if idade >= MAIOR_IDADE:
+    print("Maior de idade, pode tirar CNH.")
+else:
+    print("Menor de idade, ainda não pode tirar CNH.")
+            
