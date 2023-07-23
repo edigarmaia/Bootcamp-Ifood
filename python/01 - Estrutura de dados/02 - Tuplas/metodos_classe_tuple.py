127b0970536b7aaa3a1c7e2530ddaa3488136409
Edigar Maia <edigarmaia@gmail.com>
2023-07-23 11:10:39 -0300

    Metodos classe tuple

---
+++ python/01 - Estrutura de dados/02 - Tuplas/metodos_classe_tuple.py
@@ -0,0 +1,15 @@
+# count - conta os elementos da tupla
+cores = ("vermelho", "azul", "verde", "azul",)
+print(cores.count("vermelho"))
+print(cores.count("azul"))
+print(cores.count("verde"))
+print()
+
+# index - saber qual posição o objeto está na lista
+linguagens = ("python", "js", "c", "java", "csharp",)
+print(linguagens.index("java"))
+print(linguagens.index("python"))
+print()
+
+# len - quantos elementos tem na tupla
+print(len(linguagens))
