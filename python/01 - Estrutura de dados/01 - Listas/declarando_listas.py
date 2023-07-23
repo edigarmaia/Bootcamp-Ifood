274ac47e6cf9b4f2b829f2dd8502aa927551b37c
Edigar Maia <edigarmaia@gmail.com>
2023-07-22 21:47:42 -0300

    Declarando e acessando listas

---
+++ python/01 - Estrutura de dados/01 - Listas/declarando_listas.py
@@ -0,0 +1,17 @@
+# lista com valores
+frutas = ["laranja", "maca", "uva"]
+print(frutas)
+print(frutas[-2])
+
+# lista vazia
+frutas = []
+print(frutas)
+
+letras = list("python")
+print(letras)
+
+numeros = list(range(10))
+print(numeros)
+
+carro = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True]
+print(carro)
---
+++ python/01 - Estrutura de dados/listas.md
@@ -0,0 +1,3 @@
+Listas em python podem amazenar de maneira sequencial qualquer tipo de objeto.
+Podemos alterar seu valor após a criação.
+
