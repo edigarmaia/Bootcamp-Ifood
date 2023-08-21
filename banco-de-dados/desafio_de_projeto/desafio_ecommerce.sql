0d50dc89d67fe0733cf4f75272a2f4f57296276f
Edigar Maia <edigarmaia@gmail.com>
2023-08-21 15:48:17 -0300

    Persistindo e recuperando dados com consultas sql

--- banco-de-dados/desafio_de_projeto/desafio_ecommerce.sql
+++ banco-de-dados/desafio_de_projeto/desafio_ecommerce.sql
@@ -2,7 +2,7 @@
 
 create database ecommerce;
 use ecommerce;
-
+-- drop database ecommerce;
 -- tabela cliente
 create table clients(
 	idClient int auto_increment primary key,
@@ -10,19 +10,23 @@ create table clients(
     Minit char(3),
     Lname varchar(20),
     CPF char(11) not null,
-    Address varchar(30),
+    Address varchar(255),
     constraint unique_cpf_client unique (cPf)
 );
 desc clients;
+alter table clients auto_increment=1;
+
 -- tabela produto
 create table product(
 	idProduct int auto_increment primary key,
-    Pname varchar(10) not null,
+    Pname varchar(255) not null,
     classification_kids bool default false,
     category enum('eletrônico', 'Vestimenta', 'Brinquedos', 'Alimentos', 'Móveis') not null,
     assessment float default 0,
     size varchar(10)
 );
+
+alter table product auto_increment=1;
 -- tabela pedido
 create table orders(
 	idorder int auto_increment primary key,
@@ -34,6 +38,8 @@ create table orders(
     constraint fk_ordes_client foreign key (idorderClient) references clients(idClient)
 			on update cascade
 );
+alter table orders auto_increment=1;
+
 desc orders;
 -- tabela pagamentos
 create table payments(
@@ -43,13 +49,14 @@ create table payments(
     limitAvailable float,
     primary key(idClient, idPayment)
 );
-
+select * from orders;
 -- tabela estoque
 create table productStorage(
 	idProdStorage int auto_increment primary key,
 	storageLocation varchar(255),
     quantity int default 0
 );
+alter table productStorage auto_increment=1;
 
 -- tabela fornecedor
 create table supplier(
@@ -59,6 +66,8 @@ create table supplier(
     contact char(11) not null,
     constraint unique_supplier unique (CNPJ)
 );
+alter table supplier auto_increment=1;
+
 desc supplier;
 -- tabela vendedor
 create table seller(
@@ -72,6 +81,7 @@ create table seller(
     constraint unique_cnpj_seller unique (CNPJ),
     constraint unique_spf_seller unique (CPF)
 );
+alter table seller auto_increment=1;
 
 -- tabela produto vendedor
 create table productSeller(
@@ -123,10 +133,112 @@ show tables;
 
 
 
+-- ---------------------------------------
+-- inserção de dados e queries
 
+show tables;
+-- idClient, Fname, Minit, Lname, CPF, Address
+insert into clients (Fname, Minit, Lname, CPF, Address) 
+	   values('Maria','M','Silva', 12346789, 'rua silva de prata 29, Carangola - Cidade das flores'),
+		     ('Matheus','O','Pimentel', 987654321,'rua alemeda 289, Centro - Cidade das flores'),
+			 ('Ricardo','F','Silva', 45678913,'avenida alemeda vinha 1009, Centro - Cidade das flores'),
+			 ('Julia','S','França', 789123456,'rua lareijras 861, Centro - Cidade das flores'),
+			 ('Roberta','G','Assis', 98745631,'avenidade koller 19, Centro - Cidade das flores'),
+			 ('Isabela','M','Cruz', 654789123,'rua alemeda das flores 28, Centro - Cidade das flores');
 
+select * from clients;
+-- idProduct, Pname, classification_kids boolean, category('Eletrônico','Vestimenta','Brinquedos','Alimentos','Móveis'), avaliação, size
+insert into product (Pname, classification_kids, category, assessment, size) values
+							  ('Fone de ouvido',false,'Eletrônico','4',null),
+                              ('Barbie Elsa',true,'Brinquedos','3',null),
+                              ('Body Carters',true,'Vestimenta','5',null),
+                              ('Microfone Vedo - Youtuber',False,'Eletrônico','4',null),
+                              ('Sofá retrátil',False,'Móveis','3','3x57x80'),
+                              ('Farinha de arroz',False,'Alimentos','2',null),
+                              ('Fire Stick Amazon',False,'Eletrônico','3',null);
 
+select * from clients;
+select * from product;
+-- idOrder, idOrderClient, orderStatus, orderDescription, sendValue, paymentCash
 
+delete from orders where idOrderClient in  (1,2,3,4);
+insert into orders (idOrderClient, orderStatus, orderDescription, sendValue, paymentCash) values 
+							 (1, default,'compra via aplicativo',null,1),
+                             (2,default,'compra via aplicativo',50,0),
+                             (3,'Confirmado',null,null,1),
+                             (4,default,'compra via web site',150,0);
 
+-- idPOproduct, idPOorder, poQuantity, poStatus
+select * from orders;
+insert into productOrder (idPOproduct, idPOorder, poQuantity, poStatus) values
+						 (1,1,2,null),
+                         (2,1,1,null),
+                         (3,2,1,null);
 
+-- storageLocation,quantity
+insert into productStorage (storageLocation,quantity) values 
+							('Rio de Janeiro',1000),
+                            ('Rio de Janeiro',500),
+                            ('São Paulo',10),
+                            ('São Paulo',100),
+                            ('São Paulo',10),
+                            ('Brasília',60);
 
+-- idLproduct, idLstorage, location
+insert into storageLocation (idLproduct, idLstorage, location) values
+						 (1,2,'RJ'),
+                         (2,6,'GO');
+
+-- idSupplier, SocialName, CNPJ, contact
+insert into supplier (SocialName, CNPJ, contact) values 
+							('Almeida e filhos', 123456789123456,'21985474'),
+                            ('Eletrônicos Silva',854519649143457,'21985484'),
+                            ('Eletrônicos Valma', 934567893934695,'21975474');
+                            
+select * from supplier;
+-- idPsSupplier, idPsProduct, quantity
+insert into productSupplier (idPsSupplier, idPsProduct, quantity) values
+						 (1,1,500),
+                         (1,2,400),
+                         (2,4,633),
+                         (3,3,5),
+                         (2,5,10);
+
+-- idSeller, SocialName, AbstName, CNPJ, CPF, location, contact
+insert into seller (SocialName, AbstName, CNPJ, CPF, location, contact) values 
+						('Tech eletronics', null, 123456789456321, null, 'Rio de Janeiro', 219946287),
+					    ('Botique Durgas',null,null,123456783,'Rio de Janeiro', 219567895),
+						('Kids World',null,456789123654485,null,'São Paulo', 1198657484);
+
+select * from seller;
+-- idPseller, idPproduct, prodQuantity
+insert into productSeller (idPseller, idPproduct, prodQuantity) values 
+						 (1,6,80),
+                         (2,7,10);
+
+select * from productSeller;
+
+select count(*) from clients;
+select * from clients c, orders o where c.idClient = idOrderClient;
+
+select Fname,Lname, idOrder, orderStatus from clients c, orders o where c.idClient = idOrderClient;
+select concat(Fname,' ',Lname) as Client, idOrder as Request, orderStatus as Status from clients c, orders o where c.idClient = idOrderClient;
+
+insert into orders (idOrderClient, orderStatus, orderDescription, sendValue, paymentCash) values 
+							 (2, default,'compra via aplicativo',null,1);
+                             
+select count(*) from clients c, orders o 
+			where c.idClient = idOrderClient;
+
+select * from orders;
+
+-- recuperação de pedido com produto associado
+select * from clients c inner join orders o ON c.idClient = o.idOrderClient
+                inner join productOrder p on p.idPOorder = o.idOrder
+				group by idClient; 
+        
+-- Recuperar quantos pedidos foram realizados pelos clientes?
+select c.idClient, Fname, count(*) as Number_of_orders from clients c 
+				inner join orders o ON c.idClient = o.idOrderClient
+		group by idClient; 
+e
